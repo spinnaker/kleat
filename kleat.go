a569afb2d899503e3034c129a9ab828642bdb3ae
Eric Zimanyi <ezimanyi@google.com>
2020-01-17 14:48:56 -0500

    convert halconfig to front50

--- kleat.go
+++ kleat.go
@@ -10,16 +10,23 @@ import (
 )
 
 func main() {
-	h := proto.HalConfig{}
 	fn := os.Args[1]
 
 	dat, err := ioutil.ReadFile(fn)
 
+	h := proto.HalConfig{}
 	err = yaml.Unmarshal([]byte(dat), &h)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
-	bytes, err := yaml.Marshal(h)
+
+	// Test converting halconfig to front50config
+	f, err := halToFront50(h)
+	printObject(f)
+}
+
+func printObject(i interface{}) {
+	bytes, err := yaml.Marshal(i)
 	if err != nil {
 		panic(err)
 	}
@@ -27,4 +34,14 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func halToFront50(h proto.HalConfig) (proto.Front50Config, error) {
+	f := proto.Front50Config{
+		Spinnaker: &proto.Front50Config_Spinnaker{
+			PersistentStoreType: h.PersistentStorage.PersistentStoreType,
+			Gcs: h.PersistentStorage.Gcs,
+		},
+	}
+	return f, nil
 }
