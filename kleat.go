aa42afdb6ec57140656fab661775b49700ae270b
Eric Zimanyi <ezimanyi@google.com>
2020-01-30 16:53:45 -0500

    Put google in clouddriver config

--- kleat.go
+++ kleat.go
@@ -131,6 +131,7 @@ func extractPersistentStoreType(h proto.HalConfig) *string {
 func halToClouddriver(h proto.HalConfig) (proto.ClouddriverConfig, error) {
 	c := proto.ClouddriverConfig{
 		Kubernetes: h.Providers.Kubernetes,
+		Google: h.Providers.Google,
 	}
 	return c, nil
 }
