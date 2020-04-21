d3308054761b805a8ab1d456255d4771ac5bff2c
Eric Zimanyi <ezimanyi@google.com>
2020-04-20 20:01:35 -0400

    fix(cmd): Executable should be in package main
    When re-organizing the directories, we renamed the package of
    the main binary from main to kleat. While all importable packages
    need to be in a directory with the same name of the package, the
    entrypoint of the program must be a function main in a package
    main.
    
    the executable anymore as go programs mus

--- cmd/kleat/main.go
+++ cmd/kleat/main.go
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package kleat
+package main
 
 import (
 	"github.com/spinnaker/kleat/internal/write"
