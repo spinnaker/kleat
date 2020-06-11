7baf4530ec18c62c26b1f9860c366d49a358b4df
Eric Zimanyi <ezimanyi@google.com>
2020-06-11 16:56:48 -0400

    feat(proto): Add experimental flag
    As we've decided to use the new proto3 optional semantics to
    handle nullable fields, we need to pass this flag until the
    feature is fully GA.

--- build/protoc/genproto.sh
+++ build/protoc/genproto.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 set -e
 
+PROTOC_FLAGS="--experimental_allow_proto3_optional"
+
 gen_proto() {
   output_dir=$1
   find /proto -name *.proto | sort | xargs protoc \
+    $PROTOC_FLAGS \
     --proto_path=/proto \
     --go_out=paths=source_relative:$1/go \
     --doc_out=$1/doc \
