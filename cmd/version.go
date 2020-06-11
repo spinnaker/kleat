430920e75c268aa293cc1c0a271ada8c86bbbe7e
Maggie Neterval <mneterval@google.com>
2020-06-11 16:15:21 -0400

    feat(cmd): add Cobra framework (#90)
    * feat(cmd): add Cobra framework
    
    This commit introduces the Cobra CLI framework, and reorganizes /cmd to adhere to Cobra-recommended standards. Moves primary functionality to the generate subcommand, and introduces the version subcommand, which will print the version/commit/date as injected by goreleaser at build time.
    
    * refactor(release): remove non-default main config
    
    Now that we have a root main.go per Cobra standards, we no longer require a non-default path to our main.go to be specified as the build entrypoint.

---
+++ cmd/version.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright The Spinnaker Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// Dynamically inserted at build time by goreleaser. See `ldflags` in
+// .goreleaser.yml.
+var (
+	version = "unknown"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print kleat version",
+	Long:  "Print the version, commit, and release date of your kleat",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Printf("Kleat has version %s built from %s on %s\n", version, commit, date)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
