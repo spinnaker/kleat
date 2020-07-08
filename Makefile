2ca5264cb6a12530e29733dc9a5c01529e4dc8dc
Eric Zimanyi <ezimanyi@google.com>
2020-07-08 21:54:52 +0000

    refactor(protoc): Use better directories in container
    It's customary for mounted filesystems to be in /mnt; let's mount
    our files there instead of directly in the root of the filesystem.
    
    Rather than have a top-level staging directory, make this directory
    in /tmp. Also create it as part of the genproto script so it's
    automatically owned by whichever user is running the script.

--- Makefile
+++ Makefile
@@ -3,9 +3,9 @@ protobuilder:
 
 proto_command = docker run \
   --rm \
-  --mount type=bind,source=$(abspath api/proto/),target=/proto,readonly \
-  --mount type=bind,source=$(abspath api/client/),target=/output/go \
-  --mount type=bind,source=$(abspath docs),target=/output/doc \
+  --mount type=bind,source=$(abspath api/proto/),target=/mnt/proto,readonly \
+  --mount type=bind,source=$(abspath api/client/),target=/mnt/output/go \
+  --mount type=bind,source=$(abspath docs),target=/mnt/output/doc \
   kleat-protobuilder:latest
 
 proto: protobuilder
--- build/protoc/genproto.sh
+++ build/protoc/genproto.sh
@@ -4,25 +4,25 @@ set -e
 PROTOC_FLAGS=""
 
 gen_proto() {
-  output_dir=$1
-  find /proto -name *.proto | sort | xargs protoc \
+  mkdir -p /tmp/staging/go /tmp/staging/doc
+  find /mnt/proto -name *.proto | sort | xargs protoc \
     $PROTOC_FLAGS \
-    --proto_path=/proto \
-    --go_out=paths=source_relative:$1/go \
-    --doc_out=$1/doc \
+    --proto_path=/mnt/proto \
+    --go_out=paths=source_relative:/tmp/staging/go \
+    --doc_out=/tmp/staging/doc \
     --doc_opt=markdown,docs.md
 }
 
 update_proto() {
-  gen_proto /staging
-  rm -rf /output/go/* /output/doc/*
-  cp -r /staging/doc/* /output/doc/
-  cp -r /staging/go/* /output/go/
+  gen_proto
+  rm -rf /mnt/output/go/* /mnt/output/doc/*
+  cp -r /tmp/staging/doc/* /mnt/output/doc/
+  cp -r /tmp/staging/go/* /mnt/output/go/
 }
 
 check_proto() {
-  gen_proto /staging
-  if ! diff -r /staging /output; then
+  gen_proto
+  if ! diff -r /tmp/staging /mnt/output; then
     echo "Protocol buffer compilation out of date. Please run 'make proto' and commit the changes."
     echo "To help with debugging, the difference between the committed and generated code is printed above."
     exit 1
